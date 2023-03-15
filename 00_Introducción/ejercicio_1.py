293c070b55305b89185269c15fe36c0bede9fa3f
arosales73 <127799969+arosales73@users.noreply.github.com>
2023-03-15 12:59:07 -0600

    Update ejercicio_1.py

--- 00_Introducción/ejercicio_1.py
+++ 00_Introducción/ejercicio_1.py
@@ -1,1 +1,30 @@
+#Ejercicios 1
 
+#Introduccion
+
+print (5+2)
+print (5-2)
+print (2*2)
+print (8/3)
+print (4 **2)
+print (9 % 2)
+print (9 // 2)
+print  (3<2)
+print  (6.2 * 8.75)
+print  (3j + 1 + 2j +1)
+print ('Python')
+print  (['hola',4,2,8])
+print  ({'curso': 'Python'})
+print  ({'Hola', 3.1416, 9.2})
+print  (('Hola', 3.1416, 9.2))
+
+#Tipos de datos
+print (type (3<2))
+print ( type (5+2))
+print (type (2.5*3.8))
+print (type (3j + 1 + 2j +1))
+print  (type(('Python')))
+print  (type(['hola',4,2,8]))
+print  (type({'curso': 'Python'}))
+print  (type({'Hola', 3.1416, 9.2}))
+print  (type(('Hola', 3.1416, 9.2)))
