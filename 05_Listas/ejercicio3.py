04dc5ec42ee9c4fd9a11be26500c34c2305dbf4c
arosales73 <127799969+arosales73@users.noreply.github.com>
2023-03-24 12:09:55 -0600

    Create ejercicio3.py

---
+++ 05_Listas/ejercicio3.py
@@ -0,0 +1,41 @@
+### Ejercicios
+
+'''
+# 1. Declarar una lista vacía
+lista_vacia=[]
+
+#2. Declarar una lista con más de 5 elementos
+Lista=[1,2,3,'yo','hola','más','menos','x','y']
+
+#3. Encuentra la longitud de tu lista
+print(len(Lista))
+
+#4. Obtenga el primer elemento, el elemento del medio y el último elemento de la lista
+a=Lista[0]
+b=Lista[4]
+c=Lista [len(Lista)-1]
+print (a,b,c)
+
+#5. Declare una lista llamada tipos_de_datos_mixtos, ponga su(nombre, edad, altura, estado civil, dirección)
+tipos_de_datos_mixtos= ['Alicia del Rocio Rosales Zapata', '49','soltera','Calzada de las Animas 165 Villas del Sol']
+
+#6. Declare una variable de lista llamada it_companies y asigne los valores iniciales Facebook, Google, Microsoft, manzana, IBM, Oracle y Amazon.
+it_companies=['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle','Amazon']
+
+#7. Imprime la lista usando _print()_
+print(it_companies)
+
+# 8. Imprime el número de empresas de la lista
+print ("El numero de empras es: ", len(it_companies))
+
+# 9. Imprimir la primera, segunda y última empresa
+print(it_companies[0])
+print(it_companies[1])
+print(it_companies[len(it_companies)-1])
+
+#10. Imprime la lista después de modificar una de las empresas
+#11. Agregue una empresa de TI a it_companies
+#12. Inserte una empresa de TI en medio de la lista de empresas
+#13. Cambie uno de los nombres de it_companies a mayúsculas (IBM excluido!)
+#14. Únase a it_companies con una cadena '#;  '
+#15. Compruebe si existe una determinada empresa en la lista it_companies.
